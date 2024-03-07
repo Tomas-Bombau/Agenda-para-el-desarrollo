ed0f410644b572bfc01137c2ab73d3f893932ab9
tomas bombau <tomas.bombau@gmail.com>
2024-03-07 19:22:31 -0300

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,13 +36,13 @@ export default function Preview() {
     }}>
       <nav className="z-10 bg-transparent py-5 flex flex-col size-full sm:flex-row sm:items-center sm:justify-between 2xl:px-12 h-20">
         <div className='flex justify-between gap-5 items-center '>
-          <div className="flex items-center justify-start gap-2 min-[375px]:gap-4 lg:gap-0 sm:hidden">
+          <div className="flex items-center justify-start gap-2 min-[375px]:gap-4 lg:hidden">
             <Button onClick={() => setHamburger(!hamburger)} variant="text" size="large" iconOnly aria-label="Menu">
               <CiMenuBurger  className="stroke-inherit h-8 w-8" />
             </Button>
           </div>
           <div className='flex flex-row-reverse items-center gap-5 sm:flex-row'>
-            <div class>
+            <div className='hidden sm:flex'>
               <img className="h-10 w-10" src="https://images.vexels.com/media/users/3/142789/isolated/preview/2bfb04ad814c4995f0c537c68db5cd0b-multicolor-swirls-circle-logo.png" />
             </div>
             <Typography variant='headerLogo' fontWeight={600} style={gradientStyle}>Agenda para el Desarrollo</Typography>
@@ -50,18 +50,18 @@ export default function Preview() {
         </div>
         <div>
         </div>
-        <div className={hamburger ? "z-10 flex-col" : "hidden sm:flex sm:flex-row"}>
-          <ButtonGroup  aria-label='alignment button group' className={hamburger ? "absolute z-2 w-fit rounded z-2 mt-2 flex items-start color" : "flex flex-row mt-0 bg-hidden gap-2"}>
-            <Button href="#nosotros"  sx={{ color: 'white', border: "none", "&:hover":{textDecoration: "underline wavy",textDecorationThickness: "2px", textUnderlineOffset: "4px", transform:"scale(1.02)", border: "none"}}}>
+        <div className={hamburger ? "z-10 flex-col" : "hidden lg:flex lg:flex-row"}>
+          <ButtonGroup  aria-label='alignment button group' className={hamburger ? "absolute z-2 w-fit rounded z-2 mt-2 flex flex-col items-start color" : "flex flex-row mt-0 bg-hidden gap-2"}>
+            <Button href="#nosotros" variant="contained" color='red' sx={{ color: 'white', border: "none", "&:hover":{textDecoration: "underline wavy",textDecorationThickness: "2px", textUnderlineOffset: "4px", transform:"scale(1.02)", border: "none"}}}>
               <Typography fontWeight={400}>Nosotros</Typography>
             </Button>
-           <Button href="#nosotros" sx={{ color: 'white', border: "none", "&:hover":{textDecoration: "underline wavy",textDecorationThickness: "2px", textUnderlineOffset: "4px", transform:"scale(1.02)", border: "none"}}}>
+           <Button href="#nosotros" variant="contained" color='red' sx={{ color: 'white', border: "none", "&:hover":{textDecoration: "underline wavy",textDecorationThickness: "2px", textUnderlineOffset: "4px", transform:"scale(1.02)", border: "none"}}}>
            <Typography fontWeight={400}>Qué hacemos?</Typography>
             </Button>
-            <Button href="#nosotros" sx={{ color: 'white', border: "none", "&:hover":{textDecoration: "underline wavy",textDecorationThickness: "2px", textUnderlineOffset: "4px", transform:"scale(1.02)", border: "none"}}}>
+            <Button href="#nosotros" variant="contained" color='red' sx={{ color: 'white', border: "none", "&:hover":{textDecoration: "underline wavy",textDecorationThickness: "2px", textUnderlineOffset: "4px", transform:"scale(1.02)", border: "none"}}}>
             <Typography fontWeight={400}>Voluntariado</Typography>
             </Button>
-           <Button href="#nosotros" sx={{ color: 'white', border: "none", "&:hover":{textDecoration: "underline wavy",textDecorationThickness: "2px", textUnderlineOffset: "4px", transform:"scale(1.02)", border: "none"}}}>
+           <Button href="#nosotros" variant="contained" color='red' sx={{ color: 'white', border: "none", "&:hover":{textDecoration: "underline wavy",textDecorationThickness: "2px", textUnderlineOffset: "4px", transform:"scale(1.02)", border: "none"}}}>
            <Typography fontWeight={400}>Donar</Typography>
             </Button>
           </ButtonGroup>
